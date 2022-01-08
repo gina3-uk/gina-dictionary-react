12959e4ba47c9aa8228b4028fda6ba988630f970
gina3-uk <84281092+gina3-uk@users.noreply.github.com>
2022-01-08 13:59:23 +0000

    Added basic search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Dictionary</h1>
-      <p>Find meaning... </p>
+      <div className="container">
+        <h1>Dictionary</h1>
+        <h2>Find meaning...</h2>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Open source code created by Gina Lewis
+        </footer>
+      </div>
     </div>
   );
 }
