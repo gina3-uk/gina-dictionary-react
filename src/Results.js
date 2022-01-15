8e131860e4773b45db96644bd418331b889fda91
gina3-uk <84281092+gina3-uk@users.noreply.github.com>
2022-01-15 17:20:06 +0000

    Added phonetic and audio of searched word

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning.js";
+import Phonetic from "./Phonetic.js";
 import "./Results.css";
 
 export default function Results(props) {
@@ -10,6 +11,15 @@ export default function Results(props) {
         <h3 className="searchedWord">
           <b>{props.results.word}</b>
         </h3>
+
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
