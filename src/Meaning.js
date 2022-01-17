a29cd838b72277c8566389f54a13258d7a389bc9
gina3-uk <84281092+gina3-uk@users.noreply.github.com>
2022-01-17 19:07:23 +0000

    Added line between different definitions

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,13 +3,18 @@ import Synonyms from "./Synonyms.js";
 import "./Meaning.css";
 
 export default function Meaning(props) {
+  let line = "";
   return (
     <div className="Meaning">
       <h4 className="wordType">{props.meaning.partOfSpeech}</h4>
 
       {props.meaning.definitions.map(function (definition, index) {
+        if (index > 0) {
+          line = <hr />;
+        }
         return (
           <div key={index}>
+            {line}
             <div className="definition">{definition.definition}</div>
             <div className="example">{definition.example}</div>
             <Synonyms synonyms={definition.synonyms} />
